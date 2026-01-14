186711b6e294319e28c84b1f7922806a79a61548
jamtech <jamtechapp@gmail.com>
2026-01-14 10:33:11 +0530

    linked homepage

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header class="site-header">
-    <a href="#" class="logo">Niedrigzins24</a>
+    <a href="/" class="logo">Niedrigzins24</a>
     
     <nav class="nav-container">
         <button class="hamburger" aria-label="Toggle menu">
@@ -8,7 +8,7 @@
             <span class="bar"></span>
         </button>
         <ul class="nav-menu">
-            <li><a href="#">Startseite</a></li>
+            <li><a href="/">Startseite</a></li>
             <li><a href="#Überuns">Über uns</a></li>
             <li><a href="#kontakt">Kontakt</a></li>
         </ul>
